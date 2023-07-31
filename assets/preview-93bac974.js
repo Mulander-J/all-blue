4d7dc68659c2fe6c8c5d81f873f861c3cd1ee307
Mulander <39454158+Mulander-J@users.noreply.github.com>
2023-07-31 11:58:55 +0800

    Updates

---
+++ assets/preview-93bac974.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-ff480f03.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-a5cef85f.js"),["./DocsRenderer-3PUGWF3O-a5cef85f.js","./iframe-ff480f03.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./index-3bfdc18e.js","./index-d475d2ea.js","./_commonjs-dynamic-modules-302442b1.js","./index-d37d4223.js","./index-c7e0cd4c.js","./assert-1bda9b76.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-93bac974.js.map

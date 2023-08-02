d428dff396ed02cc680709e3c7fa661caae26add
Mulander <39454158+Mulander-J@users.noreply.github.com>
2023-08-02 15:27:40 +0800

    mod: update

---
+++ src/stories/@animation/TripleNav/TripleNav.svelte
@@ -0,0 +1,70 @@
+<script>
+  import "./index.scss";
+  import { onMount } from "svelte";
+
+  /**
+   * @type {string} Animation Type
+   */
+  export let animation = "static";
+  // static|shadow|slide
+
+  /**
+   * @type {number} Menu Count
+   */
+  export let count = 3;
+
+  let navName = 0;
+  let isChanged = false;
+  let markerElement;
+
+  /**
+   * The length of menu
+   */
+  $: menus = new Array(count || 1).fill(null).map((_, i) => `Nav_${i}`);
+  /**
+   * wheather show Indicator
+   */
+  $: showIndicator = animation !== "static";
+  /**
+   * wheather show Indicators
+   */
+  $: showIndicators = animation !== "slide";
+
+  const changeNav = (event, name) => {
+    navName = name;
+    isChanged = !isChanged; // equals markerElement.classList.toggle('changing');
+    showIndicator && animateNav(event.target);
+  };
+
+  const animateNav = (target) => {
+    if (!target || !markerElement) return;
+    markerElement.style.left = target.offsetLeft + "px";
+    markerElement.style.width = target.offsetWidth + "px";
+    markerElement.style.filter = "hue-rotate(" + Math.random() * 360 + "deg)";
+  };
+
+  onMount(() => {
+    // init default offset
+    showIndicator &&
+      markerElement &&
+      animateNav(markerElement?.parentElement?.children?.[navName]);
+  });
+</script>
+
+<nav>
+  {#each menus as m, i}
+    <li
+      class:active={i === navName}
+      on:click={(e) => changeNav(e, i)}
+      role="none"
+    >
+      {m}
+      {#if showIndicators}
+        <div class="indicators" class:isChanged class:hidden={i !== navName} />
+      {/if}
+    </li>
+  {/each}
+  {#if showIndicator}
+    <div class="indicator" class:isChanged bind:this={markerElement} />
+  {/if}
+</nav>
---
+++ src/stories/@animation/TripleNav/index.stories.js
@@ -0,0 +1,55 @@
+import TripleNav from './TripleNav.svelte';
+
+/**
+ * [Refrence:Magic Navigation Menu Indicator using CSS & Javascript](https://www.youtube.com/watch?v=k26kBQP2XH0)
+ * 
+ * Reproduce via svelte
+ */
+export default {
+  component: TripleNav,
+  argTypes: {
+    animation: {
+      control: { 
+        type: 'select',
+      },
+      options: ['static', 'slide', 'shadow']
+    },
+    count: { 
+      control: { 
+        type: 'number',
+        min: 1,
+        max: 6,
+        step: 1,
+      }
+    },
+  },
+  tags: ['autodocs']
+};
+
+/**
+ * None animation, only affect with active.
+ */
+export const Static = {
+  args: {
+    animation: 'static',
+    count: 4
+  }
+};
+/**
+ * Slide animation, slide between actived targets.
+ */
+export const Slide = {
+  args: {
+    animation: 'slide',
+    count: 4
+  }
+};
+/**
+ * Shadow animation, slide with a shadow between actived targets.
+ */
+export const Shadow = {
+  args:{
+    animation: 'shadow',
+    count: 4
+  }
+};
